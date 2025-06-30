5ad7ef2507239ec1b917153f0df2f5839526d94f
karammaarouf <karamma3rouf@gmail.com>
2025-06-30 03:45:53 -0700

    feat(auth): update login page for RTL language support
    - Changed HTML lang attribute to 'ar' and added dir='rtl'
    - Added RTL-specific styling with 'rtl' class on body
    - Adjusted form layout for right-to-left text alignment
    - Fixed show-hide password button positioning for RTL

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html lang="ar" dir="rtl">
+
+<head>
     @include('layouts.partials.head')
-  </head>
-  <body>
+</head>
+
+<body class="rtl">
     <!-- Loader starts-->
     @include('layouts.partials.loader')
     <!-- Loader ends-->
     <!-- page-wrapper Start-->
     @include('layouts.partials.messages')
-    <section>         
-      <div class="container-fluid p-0">
-        <div class="row">
-          <div class="col-12">
-            <div class="login-card">
-              <form class="theme-form login-form" method="post" action="{{ route('login') }}">
-                @csrf
-                <h4>{{ __('Login') }}</h4>
-                <h6>{{ __('Welcome back! Log in to your account.') }}</h6>
-                <div class="form-group">
-                  <label>{{ __('Email Address') }}</label>
-                  <div class="input-group"><span class="input-group-text"><i class="icon-email"></i></span>
-                    <input class="form-control @error('email') is-invalid @enderror" type="email" name="email" required="" placeholder="Test@gmail.com" value="{{ old('email') }}">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label>{{ __('Password') }}</label>
-                  <div class="input-group form-input"><span class="input-group-text"><i class="icon-lock"></i></span>
-                    <input class="form-control @error('email') is-invalid @enderror" type="password" name="password" required="" placeholder="*********">
-                    <div class="show-hide"><span class="show"></span></div>
-                    @error('email')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="checkbox">
-                    <input id="checkbox1" type="checkbox">
-                    <label for="checkbox1">{{ __('Remember password') }}</label>
-                  </div><a class="link" href="">{{ __('Forgot password?') }}</a>
+    <section>
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-12">
+                    <div class="login-card">
+                        <form class="theme-form login-form text-right" method="post" action="{{ route('login') }}">
+                            @csrf
+                            <h4>{{ __('Login') }}</h4>
+                            <h6>{{ __('Welcome back! Log in to your account.') }}</h6>
+                            <div class="form-group">
+                                <label>{{ __('Email Address') }}</label>
+                                <div class="input-group"><span class="input-group-text"><i
+                                            class="icon-email"></i></span>
+                                    <input class="form-control @error('email') is-invalid @enderror" type="email"
+                                        name="email" required="" placeholder="Test@gmail.com"
+                                        value="{{ old('email') }}">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>{{ __('Password') }}</label>
+                                <div class="input-group form-input"><span class="input-group-text"><i
+                                            class="icon-lock"></i></span>
+                                    <input class="form-control @error('email') is-invalid @enderror" type="password"
+                                        name="password" required="" placeholder="*********">
+                                    <div class="show-hide" style="right: auto; left: 30px;"><span class="show"></span></div>
+                                    @error('email')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="checkbox">
+                                    <input id="checkbox1" type="checkbox">
+                                    <label for="checkbox1">{{ __('Remember password') }}</label>
+                                </div><a class="link" href="">{{ __('Forgot password?') }}</a>
 
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary btn-block" type="submit">{{ __('Sign in') }}</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div class="form-group">
-                  <button class="btn btn-primary btn-block" type="submit">{{ __('Sign in') }}</button>
-                </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
     </section>
     <!-- latest jquery-->
     @include('layouts.partials.scripts')
-  </body>
+</body>
+
 </html>
